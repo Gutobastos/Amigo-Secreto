5fe08ba8e61a141dfa9f532196608f775d7a2fc9
Fabricio Augusto Oliveira Bastos <fabricio.bastos.ba@gmail.com>
2025-01-23 18:59:21 -0300

    Alteração de imagem, carregamento de pasta img,

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaNomes = [];
+console.log(listaNomes);
+
+//Função para exibir textos nas TAGs
+function exibirNomes(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+//Função para incluir nomes na lista de amigos
+function adicionarAmigo() {
+    let addnome = document.querySelector('input').value;
+
+    if (addnome == '') {
+        alert('ERROR: Por favor inserir nome!');
+    } else {
+            listaNomes.push(addnome);
+            exibirNomes('ul', listaNomes)
+            limpaCampo();
+        }
+}
+
+//Função para sortear um nome da lista de nomes de amigos
+function sortearAmigo() {
+    let nomeEscolhido = parseInt(Math.random() * listaNomes.length);
+    console.log(nomeEscolhido);
+    exibirNomes('ul', 'Seu amigo sorteado é: ' + listaNomes[nomeEscolhido]);
+    console.log(listaNomes);
+}
+
+//Função para limpar campo INPUT
+function limpaCampo() {
+    document.querySelector('input').value = '';
+}
